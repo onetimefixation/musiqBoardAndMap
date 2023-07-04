a0ce036266ecadabfac3b39e8700cbf39fb5432e
onetimefixation <onetimefixation@gmail.com>
2023-07-04 14:53:53 -0400

    add KEY code v1

---
+++ getKInfo.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.get('/api/apiKey', (req, res) => {
+  const apiKey = process.env.API_KEY;
+
+  if (!apiKey) {
+    return res.status(500).json({ error: 'API key not found' });
+  }
+
+  res.json({ apiKey });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
