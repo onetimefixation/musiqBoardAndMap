6bbf76ab625660fe055bf4a79f290e126f77b466
onetimefixation <onetimefixation@gmail.com>
2023-07-09 19:40:53 -0400

    added necessary code to add / to server and package files

--- getKInfo.js
+++ getKInfo.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/* const express = require('express');
 const app = express();
 
 // https://musiqboardmap-c63a7b9c472d.herokuapp.com/api/apiKey
@@ -8,6 +8,33 @@ app.get('/', (req, res) => {
     res.send('Welcome to the server')
 })
 
+
+app.get('/api/apiKey', (req, res) => {
+  const apiKey = process.env.API_KEY;
+
+  if (!apiKey) {
+    return res.status(500).json({ error: 'API key not found' });
+  }
+
+  res.json({ apiKey });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+ */
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the root directory
+app.use(express.static(path.join(__dirname)));
+
+// Handle requests for the root URL
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.get('/api/apiKey', (req, res) => {
   const apiKey = process.env.API_KEY;
