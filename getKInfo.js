579af03a5a22cdda4ceb410be668501ae673a88a
onetimefixation <onetimefixation@gmail.com>
2023-07-09 20:03:32 -0400

    cleaned up index & getKInfo and correct the api path in map.htmp

--- getKInfo.js
+++ getKInfo.js
@@ -1,29 +1,5 @@
-/* const express = require('express');
-const app = express();
-
 // https://musiqboardmap-c63a7b9c472d.herokuapp.com/api/apiKey
-// /api/apiKey
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the server')
-})
-
-
-app.get('/api/apiKey', (req, res) => {
-  const apiKey = process.env.API_KEY;
-
-  if (!apiKey) {
-    return res.status(500).json({ error: 'API key not found' });
-  }
-
-  res.json({ apiKey });
-});
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
- */
 const express = require('express');
 const path = require('path');
 const app = express();
