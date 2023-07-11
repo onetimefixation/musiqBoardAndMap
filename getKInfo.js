aa3b38999ec631d900ab7f9d27398347ce84a645
onetimefixation <onetimefixation@gmail.com>
2023-07-10 20:19:46 -0400

    fixed api keys

--- getKInfo.js
+++ getKInfo.js
@@ -13,14 +13,20 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/apiKey', (req, res) => {
-  const apiKey = process.env.API_KEY;
-
-  if (!apiKey) {
-    return res.status(500).json({ error: 'API key not found' });
-  }
-
-  res.json({ apiKey });
-});
+    const apiKey = process.env.API_KEY;
+    const apiKey_checkwx = process.env.API_KEY_CHECKWX;
+  
+    if (!apiKey) {
+      return res.status(500).json({ error: 'API key not found' });
+    }
+  
+    if (!apiKey_checkwx) {
+      return res.status(500).json({ error: 'apiKey_checkwx not found' });
+    }
+  
+    res.json({ apiKey, apiKey_checkwx });
+  });
+  
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
